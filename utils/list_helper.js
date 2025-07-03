9780f6c598d3a1aa873d281a0af71df67cafe764
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-03 23:26:12 +0100

    Exercise 4.5 - favoriteBlog test created

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    const result = blogs.reduce(reducer, 0)
+    return result
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
 }
