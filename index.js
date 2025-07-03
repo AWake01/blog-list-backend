88b49a84d8d243ca10a618793333d49afeca0b82
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-03 17:56:04 +0100

    Exercise 4.1 - Basic api testing completed with VS Code REST client

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const mongoose = require('mongoose')
 const app = express()
 
 require('dotenv').config()
+app.use(express.json())
 const url = process.env.MONGODB_URI
 
+//MONGODB
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
+})
+
+blogSchema.set('toJSON', {  //Convert id to string and remove unused fields in response
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
@@ -20,12 +30,16 @@ mongoose.connect(url)
 
 app.use(express.json())
 
+//GET
+//all
 app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs)
   })
 })
 
+//POST
+//blog
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
@@ -34,6 +48,7 @@ app.post('/api/blogs', (request, response) => {
   })
 })
 
+//CONNECTION
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
