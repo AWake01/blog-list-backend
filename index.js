5abe30f0c46caa0bae27f3e77da9e19f502db7d6
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-03 19:01:19 +0100

    Exercise 4.2 - Added controllers/blogs.js

--- index.js
+++ index.js
@@ -14,24 +14,6 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
-//GET
-//all
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-//POST
-//blog
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
 //CONNECTION
 const PORT = config.PORT
 app.listen(PORT, () => {
