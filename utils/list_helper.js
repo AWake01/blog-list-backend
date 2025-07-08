dd24a9dbc7dd55029a34858b51908f80c7efb831
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-08 22:37:11 +0100

    Exercise 4.86- complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -14,8 +16,38 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
 }
 
+
+const mostBlogs = (blogs) => {
+    let bestAuthor = _.maxBy(blogs, 'author')
+    console.log('author ', bestAuthor)
+    //let blogCount = blogs.filter(blog => blog.author === bestAuthor).length
+    // return {
+    //     author: bestAuthor.author,
+    //     blogs: blogCount
+    // }
+
+//     let blogCount = _.maxBy(
+//   _.map(_.countBy(listWithMultipleBlogs, 'author'), (count, val) => ({
+//     author: val,
+//     blogs: count,
+//   })),
+//   'blogs'
+// );
+
+    
+   let mostBlogs= _.maxBy(_.map(_.countBy(blogs, 'author'), (count, val) => ({ author: val, blogs: count })), 'blogs')
+
+   console.log('most' , mostBlogs)
+   
+   return mostBlogs
+
+    //let freq = _.countBy(blogs)
+    //console.log('freq ', freq)
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs
 }
