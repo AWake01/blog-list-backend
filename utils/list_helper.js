4960da244c2f5d01c67fa0c6685d08eea460b5a3
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-09 14:39:15 +0100

    Exercise 4.7 complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -18,36 +18,31 @@ const favoriteBlog = (blogs) => {
 
 
 const mostBlogs = (blogs) => {
-    let bestAuthor = _.maxBy(blogs, 'author')
-    console.log('author ', bestAuthor)
-    //let blogCount = blogs.filter(blog => blog.author === bestAuthor).length
-    // return {
-    //     author: bestAuthor.author,
-    //     blogs: blogCount
-    // }
-
-//     let blogCount = _.maxBy(
-//   _.map(_.countBy(listWithMultipleBlogs, 'author'), (count, val) => ({
-//     author: val,
-//     blogs: count,
-//   })),
-//   'blogs'
-// );
-
-    
+ 
    let mostBlogs= _.maxBy(_.map(_.countBy(blogs, 'author'), (count, val) => ({ author: val, blogs: count })), 'blogs')
-
-   console.log('most' , mostBlogs)
-   
    return mostBlogs
-
-    //let freq = _.countBy(blogs)
-    //console.log('freq ', freq)
 }
+
+const mostLikes = (blogs) => {
+   //https://stackoverflow.com/questions/38774763/using-lodash-to-sum-values-by-key
+   let mostLikes = 
+        _(blogs)
+        .groupBy('author')
+        .map((object, key) => ({
+            'author': key,
+            'likes': _.sumBy(object, 'likes')
+        })).maxBy('likes')
+
+   console.log('mostLines ', mostLikes)
+   return mostLikes
+}
+
+
 
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
