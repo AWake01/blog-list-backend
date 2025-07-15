9a2965ee8a452cc7a65d58e9f9b9c28b0cb14c94
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-07-15 16:18:22 +0100

    Exercise 4.10 complete: POST validated and made async

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -8,6 +8,7 @@ const app = require('../app')
 const Blog = require('../models/blog')
 
 const helper = require('./test_helper')
+const { initialNotes } = require('../../../part3 Programming a server with NodeJS and Express/partA/REST/tests/test_helper')
 const api = supertest(app)
 
 //INITALISE DATA
@@ -36,6 +37,31 @@ describe('GET requests', () => {
         });
 
         //assert.strictEqual(response.body[0].hasOwnProperty('id'), true)
+    })
+})
+
+//POST requests
+describe('POST requests', () => {
+    test('POST adds a new blog', async () => {     //Pass
+        const newBlog = {
+            title: "New Blog",
+            author: "New Author ",
+            url: "https://newBlog.com/",
+            likes: 5,
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const titles = response.body.map(blog => blog.title)
+
+        assert.strictEqual(response.body.length, helper.initalBlogs.length + 1)
+        assert(titles.includes('New Blog'))
     })
 })
 
