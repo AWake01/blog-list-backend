0f54c87869eb73278f5da55820b2bcf3ba26d294
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-09-12 16:33:48 +0100

    Exercise 5.18 complete: Added EtE tests for login

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use('/api/blogs', middleware.userExtractor, blogsRouter)  //userExtractor is
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+//Register testing router for database reset only when in test mode
+if(process.env.NODE_ENV === 'test'){
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)  
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
