32368b8135cffbfae57b979fb08c8e15bdaacaff
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-09-19 14:51:44 +0100

    Added cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ mongoose
     logger.error('error connection to MongoDB:', error.message)
   })
 
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
