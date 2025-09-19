7f2cc8783034d71e11716d27b1acca42b7f9bfdd
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-09-19 15:00:00 +0100

    Deployment fix part 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 //CONNECTION
-const PORT = config.PORT
+const PORT = config.PORT || 3001
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
